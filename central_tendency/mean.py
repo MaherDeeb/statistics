c30b60189acfeb884120bf992c8c7c48e2c03c68
Maher Deeb <maherdeebcv@gmail.com>
2020-12-25 18:10:01 +0100

    initial commit

---
+++ central_tendency/mean.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+
+def mean(x):
+    return np.sum(x) / np.dim(x)
---
+++ tests/test_central_tendency.py
@@ -0,0 +1,12 @@
+import unittest
+import numpy as np
+from central_tendency.mean import mean
+
+
+class MyTestCase(unittest.TestCase):
+    def test_mean(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
